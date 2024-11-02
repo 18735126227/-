434a6b19e21605ca5ed7e56790ee00b9f5014762
18735126227 <100126311+18735126227@users.noreply.github.com>
2024-11-02 10:39:17 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
-#include "QtWidgetsApplication.h"
+#include "QtMainWindow.h"
 #include <QtWidgets/QApplication>
+//#include <QTextCodec>
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QtWidgetsApplication w;
+   a.setFont(QFont("Microsoft yahei", 9));
+   //…Ë÷√÷–Œƒ±‡¬Î
+
+   //QTextCode* codec = QTextCodec::codecForName("utf-8");
+   //QTextCode::setCodecForLocale(codec);
+
+
+    QtMainWindow w;
     w.show();
     return a.exec();
 }
